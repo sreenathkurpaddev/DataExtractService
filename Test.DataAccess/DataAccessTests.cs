fffa8129684c805c4249da799a6ff518264bf6d4
sreenathkurpad <sreenathkurpad@hotmail.com>
2016-12-25 16:26:49 +0000

    Added a test for DAL

--- Test.DataAccess/DataAccessTests.cs
+++ Test.DataAccess/DataAccessTests.cs
@@ -1,14 +1,19 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+using DataExtractService.DataAccess.Contracts;
+using DataExtractService.DataAccess.Implementation;
+using DataExtractService.Objects;
 namespace Test.DataAccess
 {
     [TestClass]
     public class DataAccessTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void GetKeyeventsforProcess()
         {
+            IDataAccess _dal = new DataAccessImpl();
+            var records =_dal.GetKeyEventsToProcessAsync();
+            Assert.IsTrue(records.Result!= null);
         }
     }
 }
